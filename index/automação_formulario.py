9d884151af88b3b62cff466f1faf68b543d16232
Alisson Rodrigo <alissorodrigo098@gmail.com>
2023-08-11 22:30:08 -0300

    wait

--- index/automação_formulario.py
+++ index/automação_formulario.py
@@ -12,34 +12,36 @@ while True:
         navegador = webdriver.Chrome()
         actions = ActionChains(navegador)
         navegador.get("https://www.google.com/travel/flights?sca_esv=555979541&output=search&q=passagens+aereas&source=lnms&mode_promoted=true&impression_in_search=true&sa=X&sqi=2&ved=2ahUKEwjtxKnJm9WAAxXfLrkGHXdLBN4Q0pQJegQICRAB")
+
         origem = navegador.find_element('xpath','//*[@id="i21"]/div[1]/div/div/div[1]/div/div/input')
         actions.click(origem)
         actions.send_keys_to_element(origem, "\b")
         actions.send_keys('Rio de Janeiro')
         actions.send_keys(Keys.ENTER)
         actions.perform()
-        sleep(3)
+        WebDriverWait(navegador, 10).until(EC.presence_of_element_located(('xpath','//*[@id="i21"]/div[4]/div/div/div[1]/div/div/input')))
          
         destino = navegador.find_element('xpath','//*[@id="i21"]/div[4]/div/div/div[1]/div/div/input')
         actions.click(destino)
         actions.send_keys('Sao paulo')
         actions.send_keys(Keys.ENTER)
         actions.perform()
-        sleep(3)
-        
+        WebDriverWait(navegador, 10).until(EC.presence_of_element_located(('xpath','//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/input')))
+    
+
         ida = navegador.find_element('xpath','//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/input')
         actions.click(ida)
         actions.send_keys('01/01/2024')
         actions.send_keys(Keys.ENTER)
         actions.perform()
-        sleep(3)
-
+        WebDriverWait(navegador, 10).until(EC.presence_of_element_located(('xpath','//*[@id="ow81"]/div[2]/div/div[3]/div[3]/div/button')))
+  
         volta = navegador.find_element('xpath','//*[@id="ow81"]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div/input')
         actions.click(volta)
         actions.send_keys('01/02/2024')
         actions.send_keys(Keys.ENTER)
         actions.perform()
-        sleep(3)
+        WebDriverWait(navegador, 10).until(EC.presence_of_element_located(('xpath','//*[@id="ow81"]/div[2]/div/div[3]/div[3]/div/button')))
 
         confirmar_data = navegador.find_element('xpath','//*[@id="ow81"]/div[2]/div/div[3]/div[3]/div/button')
         confirmar_data.click()
@@ -83,7 +85,8 @@ while True:
             print('-------------------')
             print('')
             count += 1
-        sleep(1000)
+        navegador.quit()
+        break
     except:
-        sleep(100)
+        print('Erro ao carregar a página, tentando novamente...')
         continue
