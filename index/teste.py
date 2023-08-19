9cbe95e601167ed022c49d6e8397cb467893b96e
Alisson Rodrigo <alissorodrigo098@gmail.com>
2023-08-18 22:44:39 -0300

    ok

--- index/teste.py
+++ index/teste.py
@@ -64,7 +64,7 @@ class Main(QtWidgets.QMainWindow, Ui_Main):
 
     def mostrar_tela_principal(self):
         self.QtStack.setCurrentIndex(1)
-        if self.dados['voo1'] in self.dados:
+        if self.dados in self.dados['voo1']:
             self.tela_principal.lineEdit.setText(self.dados['voo1']['companhia'])
             self.tela_principal.lineEdit_2.setText(self.dados['voo1']['preço'])
             self.tela_principal.lineEdit_3.setText(self.dados['voo1']['horario_partida'])
@@ -75,7 +75,7 @@ class Main(QtWidgets.QMainWindow, Ui_Main):
             self.tela_principal.lineEdit_3.setText("Não há voos disponíveis")
             self.tela_principal.lineEdit_4.setText("Não há voos disponíveis")
 
-        if self.dados['voo2'] in self.dados:
+        if self.dados in self.dados['voo2']:
             self.tela_principal.lineEdit_5.setText(self.dados['voo2']['companhia'])
             self.tela_principal.lineEdit_6.setText(self.dados['voo2']['preço'])
             self.tela_principal.lineEdit_7.setText(self.dados['voo2']['horario_partida'])
@@ -87,7 +87,7 @@ class Main(QtWidgets.QMainWindow, Ui_Main):
             self.tela_principal.lineEdit_8.setText("Não há voos disponíveis")
         
 
-        if self.dados['voo3' in self.dados]:
+        if self.dados in self.dados['voo3']:
             self.tela_principal.lineEdit_9.setText(self.dados['voo3']['companhia'])
             self.tela_principal.lineEdit_10.setText(self.dados['voo3']['preço'])
             self.tela_principal.lineEdit_11.setText(self.dados['voo3']['horario_partida'])
@@ -98,7 +98,7 @@ class Main(QtWidgets.QMainWindow, Ui_Main):
             self.tela_principal.lineEdit_11.setText("Não há voos disponíveis")
             self.tela_principal.lineEdit_12.setText("Não há voos disponíveis")
 
-        if self.dados['voo4'] in self.dados:
+        if self.dados in self.dados['voo4']:
             self.tela_principal.lineEdit_13.setText(self.dados['voo4']['companhia'])
             self.tela_principal.lineEdit_14.setText(self.dados['voo4']['preço'])
             self.tela_principal.lineEdit_15.setText(self.dados['voo4']['horario_partida'])
@@ -109,7 +109,7 @@ class Main(QtWidgets.QMainWindow, Ui_Main):
             self.tela_principal.lineEdit_15.setText("Não há voos disponíveis")
             self.tela_principal.lineEdit_16.setText("Não há voos disponíveis")
 
-        if self.dados['voo5'] in self.dados:
+        if self.dados in self.dados['voo5']:
             self.tela_principal.lineEdit_17.setText(self.dados['voo5']['companhia'])
             self.tela_principal.lineEdit_18.setText(self.dados['voo5']['preço'])
             self.tela_principal.lineEdit_19.setText(self.dados['voo5']['horario_partida'])
