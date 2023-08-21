ed0e6ae8a6ca6e84ddf72a36fce5100c193879db
JorgeLuis8 <jorge.luz@ufpi.edu.br>
2023-08-21 07:58:12 -0300

    te

--- FlightPrice/teste.py
+++ FlightPrice/teste.py
@@ -1,5 +1,5 @@
 from classes import FlightPrice
-teste = FlightPrice('São Paulo','Rio de janeiro','30/8/2023','30/9/2023').buscar_voos()
+teste = FlightPrice('São Paulo','Salvador','30/8/2023','30/9/2023').buscar_voos()
 
 
 print(teste)
