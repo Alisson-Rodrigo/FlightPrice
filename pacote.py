7f59b42184aafe3221f387722cec6cb175238c0f
Alisson Rodrigo <alissorodrigo098@gmail.com>
2023-08-21 23:02:44 -0300

    ok

--- pacote.py
+++ pacote.py
@@ -1,4 +1,4 @@
 import FlightPrice
-pacote = FlightPrice.Melhores_voos("Teresina", "São paulo", "17/11/2023", "20/12/2023")
+pacote = FlightPrice.Melhores_voos("Picos", "São paulo", "17/11/2023", "20/12/2023")
 var = pacote.buscar_voos()
 print (var)
