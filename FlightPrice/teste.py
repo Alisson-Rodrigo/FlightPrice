ac479bb858990100b1288396c8d78c8c7b5c6bc4
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-09-13 08:15:27 -0300

    n

--- FlightPrice/teste.py
+++ FlightPrice/teste.py
@@ -1,1 +1,9 @@
-from classes import Melhores_preços
+class Melhores_voos:
+    def __init__(self, origem, destino, ida, volta):
+        self.origem = origem
+        self.destino = destino
+        self.ida = ida
+        self.volta = volta
+
+    def __str__(self):
+        return f"Rota: {self.origem} -> {self.destino}\nIda: {self.ida}\nVolta: {self.volta}"
